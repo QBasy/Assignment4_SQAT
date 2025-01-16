b1d46dc294644147458918560d304fbeb00be700
QoidynBasy <c0mplexie1@gmail.com>
2025-01-16 19:37:32 +0500

    Aviata Model Controller Changes

--- pom/aviata_home.go
+++ pom/aviata_home.go
@@ -5,116 +5,160 @@ import (
 	"time"
 )
 
-// AviataHomePage defines the structure for Aviata's home page.
-type AviataHomePage struct {
-	driver        selenium.WebDriver
-	datePicker    selenium.WebElement
-	toCityInput   selenium.WebElement
-	fromCityInput selenium.WebElement
-	searchButton  selenium.WebElement
+type AviataSearchForm struct {
+	driver selenium.WebDriver
 }
 
-// NewAviataHomePage initializes a new instance of AviataHomePage.
-func NewAviataHomePage(driver selenium.WebDriver) (*AviataHomePage, error) {
+type SearchFormElements struct {
+	FromInput       selenium.WebElement
+	ToInput         selenium.WebElement
+	FromCityCode    selenium.WebElement
+	ToCityCode      selenium.WebElement
+	DepartureDate   selenium.WebElement
+	ReturnDate      selenium.WebElement
+	PassengerClass  selenium.WebElement
+	SearchButton    selenium.WebElement
+	ComplexRouteBtn selenium.WebElement
+	AnywhereBtn     selenium.WebElement
+	BookingCheckbox selenium.WebElement
+}
+
+func NewAviataSearchForm(driver selenium.WebDriver) *AviataSearchForm {
+	return &AviataSearchForm{
+		driver: driver,
+	}
+}
+
+func (a *AviataSearchForm) GetElements() (*SearchFormElements, error) {
+	elements := &SearchFormElements{}
 	var err error
-	aviataHomePage := &AviataHomePage{driver: driver}
-	if aviataHomePage.fromCityInput, err = driver.FindElement(selenium.ByXPATH, "//div[contains(@class, 'search')]//input[1]"); err != nil {
-		return nil, err
-	}
-	if aviataHomePage.toCityInput, err = driver.FindElement(selenium.ByXPATH, `//*[@id="search-route-0"]/div[3]/label/input`); err != nil {
-		return nil, err
-	}
-	if aviataHomePage.datePicker, err = driver.FindElement(selenium.ByCSSSelector, ".date-picker-class"); err != nil {
-		return nil, err
-	}
-	if aviataHomePage.searchButton, err = driver.FindElement(selenium.ByCSSSelector, "button[type='submit']"); err != nil {
+
+	elements.FromInput, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"#search-route-0 input[placeholder='Откуда']")
+	if err != nil {
 		return nil, err
 	}
 
-	return aviataHomePage, nil
+	elements.ToInput, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"#search-route-0 input[placeholder='Куда']")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.FromCityCode, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"#search-route-0 div:first-child span.text-gray-500")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.ToCityCode, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"#search-route-0 div:nth-child(3) span.text-gray-500")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.DepartureDate, err = a.driver.FindElement(selenium.ByID, "desktop-main-date-from")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.ReturnDate, err = a.driver.FindElement(selenium.ByID, "desktop-main-date-to")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.PassengerClass, err = a.driver.FindElement(selenium.ByCSSSelector,
+		".extra-params .relative")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.SearchButton, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"button[type='submit']")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.ComplexRouteBtn, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"button:contains('Составить сложный маршрут')")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.AnywhereBtn, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"button:contains('Куда угодно')")
+	if err != nil {
+		return nil, err
+	}
+
+	elements.BookingCheckbox, err = a.driver.FindElement(selenium.ByCSSSelector,
+		"input[type='checkbox']")
+	if err != nil {
+		return nil, err
+	}
+
+	return elements, nil
 }
 
-// NavigateToHome navigates to the Aviata home page.
-func (p *AviataHomePage) NavigateToHome() error {
-	return p.driver.Get("https://aviata.kz")
-}
-
-// GetFromCityInput returns the 'From City' input element.
-func (p *AviataHomePage) GetFromCityInput() (selenium.WebElement, error) {
-	return p.driver.FindElement(selenium.ByXPATH, "//div[contains(@class, 'search')]//input[1]")
-}
-
-// GetToCityInput returns the 'To City' input element.
-func (p *AviataHomePage) GetToCityInput() (selenium.WebElement, error) {
-	return p.driver.FindElement(selenium.ByXPATH, `//*[@id="search-route-0"]/div[3]/label/input`)
-}
-
-// GetDatePicker returns the date picker element.
-func (p *AviataHomePage) GetDatePicker() (selenium.WebElement, error) {
-	return p.driver.FindElement(selenium.ByCSSSelector, ".date-picker-class")
-}
-
-// GetSearchButton returns the Search Button element.
-func (p *AviataHomePage) GetSearchButton() (selenium.WebElement, error) {
-	return p.driver.FindElement(selenium.ByCSSSelector, "button[type='submit']")
-}
-
-func (p *AviataHomePage) SetFromCity(city string) error {
-	fromCityInput, err := p.GetFromCityInput()
+func (a *AviataSearchForm) SearchFlight(from, to, departDate string) error {
+	elements, err := a.GetElements()
 	if err != nil {
 		return err
 	}
-	if err = fromCityInput.Click(); err != nil {
+
+	if err := elements.FromInput.Clear(); err != nil {
 		return err
 	}
-	time.Sleep(1 * time.Second)
+	if err := elements.FromInput.SendKeys(from); err != nil {
+		return err
+	}
+	time.Sleep(500 * time.Millisecond)
 
-	cityOption, err := p.driver.FindElement(selenium.ByXPATH, `//*[@id="search-route-0"]/div[1]/div[2]/div/ul/li[2]/div/div`)
+	if err := elements.ToInput.Clear(); err != nil {
+		return err
+	}
+	if err := elements.ToInput.SendKeys(to); err != nil {
+		return err
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	if err := elements.DepartureDate.Click(); err != nil {
+		return err
+	}
+	time.Sleep(500 * time.Millisecond)
+
+	dateElement, err := a.driver.FindElement(selenium.ByCSSSelector,
+		"[data-date='"+departDate+"']")
 	if err != nil {
 		return err
 	}
-	return cityOption.Click()
+	if err := dateElement.Click(); err != nil {
+		return err
+	}
+
+	return elements.SearchButton.Click()
 }
 
-func (p *AviataHomePage) SetToCity(city string) error {
-	toCityInput, err := p.GetToCityInput()
+func (a *AviataSearchForm) ToggleBookingSearch() error {
+	elements, err := a.GetElements()
 	if err != nil {
 		return err
 	}
-	if err = toCityInput.Click(); err != nil {
-		return err
-	}
-	time.Sleep(2 * time.Second)
-
-	cityOption, err := p.driver.FindElement(selenium.ByXPATH, `//*[@id="search-route-0"]/div[3]/div[2]/div/ul/li[1]/div/div`)
-	if err != nil {
-		return err
-	}
-	return cityOption.Click()
+	return elements.BookingCheckbox.Click()
 }
 
-// SelectDate sets the travel date.
-func (p *AviataHomePage) SelectDate(date string) error {
-	datePicker, err := p.GetDatePicker()
+func (a *AviataSearchForm) OpenComplexRoute() error {
+	elements, err := a.GetElements()
 	if err != nil {
 		return err
 	}
-	if err = datePicker.Click(); err != nil {
-		return err
-	}
-	time.Sleep(1 * time.Second)
-
-	dateOption, err := p.driver.FindElement(selenium.ByXPATH, `//div[@data-date='`+date+`']`)
-	if err != nil {
-		return err
-	}
-	return dateOption.Click()
+	return elements.ComplexRouteBtn.Click()
 }
 
-func (p *AviataHomePage) SearchFlights() error {
-	searchButton, err := p.GetSearchButton()
+func (a *AviataSearchForm) OpenAnywhereSearch() error {
+	elements, err := a.GetElements()
 	if err != nil {
 		return err
 	}
-	return searchButton.Click()
+	return elements.AnywhereBtn.Click()
 }
